ad6a6dc21d67cebb60aa787e76892bf0d2e446e3
ffekete <ffekete84@gmail.com>
2021-01-29 16:06:10 +0000

    better attack animations

--- core/src/com/mygdx/game/item/OneHandedSword.java
+++ core/src/com/mygdx/game/item/OneHandedSword.java
@@ -1,16 +1,8 @@
 package com.mygdx.game.item;
 
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.mygdx.game.actor.AbstractActor;
 import com.mygdx.game.graph.AtlasUnpacker;
 
-public class OneHandedSword extends Equipment {
-
-    private TextureRegion textureRegion;
-    private float rotation = -250f;
-    private int attackDelay = 0;
+public class OneHandedSword extends Weapon {
 
     public OneHandedSword() {
         super(null, null, null);
@@ -18,7 +10,7 @@ public class OneHandedSword extends Equipment {
 
     public OneHandedSword(String name, String id, String description) {
         super(name, id, description);
-        this.textureRegion = AtlasUnpacker.I.atlas.findRegion(String.format("items/weapons/%s", id));
+        super.textureRegion = AtlasUnpacker.I.atlas.findRegion(String.format("items/weapons/%s", id));
     }
 
     @Override
@@ -26,65 +18,7 @@ public class OneHandedSword extends Equipment {
         return new OneHandedSword(this.name, this.id, this.description);
     }
 
-    @Override
-    public void draw(SpriteBatch batch, AbstractActor actor, float xOffset, float yOffset, boolean flip) {
-        switch (actor.state) {
 
-            case Idle:
-            case Walking:
-                batch.setColor(Color.WHITE);
-                float calculatedXOffset = (flip ? -1 : 1) * (textureRegion.getRegionWidth() / 4f);
-                float calculatedYOffset = (actor.getCharacterAnimation().meta.height / 5f);
-                batch.draw(textureRegion.getTexture(), actor.x + calculatedXOffset + xOffset, actor.y + calculatedYOffset + yOffset, 0, 0, textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), 1, 1, 0, textureRegion.getRegionX(), textureRegion.getRegionY(), textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), flip, false);
-                break;
-            case Attacking:
 
-                calculateRotation(actor, flip);
 
-                batch.setColor(Color.WHITE);
-                if(flip) {
-                    calculatedXOffset = 5;
-                } else {
-                    calculatedXOffset = (textureRegion.getRegionWidth()) - 5;
-                }
-
-                if(flip) {
-                    calculatedYOffset = (actor.getCharacterAnimation().meta.height / 3f);
-                } else {
-                    calculatedYOffset = (actor.getCharacterAnimation().meta.height / 5f);
-                }
-
-                batch.draw(textureRegion.getTexture(), actor.x + calculatedXOffset + xOffset, actor.y + calculatedYOffset + yOffset, 0, textureRegion.getRegionHeight() / 2f, textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), 1, 1, rotation, textureRegion.getRegionX(), textureRegion.getRegionY(), textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), false, flip);
-
-                break;
-            case Dead:
-
-                break;
-            default:
-
-        }
-    }
-
-    private void calculateRotation(AbstractActor actor, boolean flip) {
-        if(attackDelay > 0) {
-            attackDelay--;
-        }
-        else {
-            if(!flip) {
-                rotation -= 7f;
-                if (rotation <= -380) {
-                    rotation = -250;
-                    attackDelay = actor.getAttackSpeed();
-                    // todo attack calc trigger here
-                }
-            } else {
-                rotation += 7f;
-                if (rotation >= -160) {
-                    rotation = -290;
-                    attackDelay = actor.getAttackSpeed();
-                    // todo attack calc trigger here
-                }
-            }
-        }
-    }
 }
---
+++ core/src/com/mygdx/game/item/Weapon.java
@@ -0,0 +1,165 @@
+package com.mygdx.game.item;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.mygdx.game.actor.AbstractActor;
+import com.mygdx.game.graph.AtlasUnpacker;
+
+import java.util.Random;
+
+public class Weapon extends Equipment {
+
+    protected TextureRegion textureRegion;
+
+    public Weapon() {
+        super(null, null, null);
+    }
+
+    private float rotation = -250f;
+    private int attackDelay = 0;
+    private int attackAnimation = 0;
+    private float attackXOffset = 5;
+    boolean flipX = false;
+    Boolean flipY = null;
+
+    public Weapon(String name, String id, String description) {
+        super(name, id, description);
+        this.textureRegion = AtlasUnpacker.I.atlas.findRegion(String.format("items/weapons/%s", id));
+    }
+
+    @Override
+    public Item cloneItem() {
+        return new Weapon(this.name, this.id, this.description);
+    }
+
+    @Override
+    public void draw(SpriteBatch batch, AbstractActor actor, float xOffset, float yOffset, boolean flip) {
+        if(flipY == null) {
+            // first anim ever, need to decide if flipY needed
+            this.flipY = flip ? true : false;
+        }
+        switch (actor.state) {
+
+            case Idle:
+            case Walking:
+                batch.setColor(Color.WHITE);
+                float calculatedXOffset = (actor.getCharacterAnimation().meta.width / 3f);
+                float calculatedYOffset = (actor.getCharacterAnimation().meta.height / 4f);
+                batch.draw(textureRegion.getTexture(), actor.x - calculatedXOffset - xOffset, actor.y + calculatedYOffset + yOffset, 0, 0, textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), 1, 1, 0, textureRegion.getRegionX(), textureRegion.getRegionY(), textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), flip, false);
+                break;
+            case Attacking:
+                if (attackAnimation == 0) {
+                    calculateRotation(actor, flip);
+                } else {
+                    calculateOffset(actor, flip);
+                }
+                batch.setColor(Color.WHITE);
+                calculatedXOffset = -actor.getCharacterAnimation().meta.width / 2f;
+                calculatedYOffset = (actor.getCharacterAnimation().meta.height / 4f);
+
+                // flip only if attack is ready
+                if(attackDelay == 0) {
+                    if (flip) {
+                        flipX = (attackAnimation == 0 ? !flip : flip);
+                        flipY = (attackAnimation == 0 ? flip : !flip);
+                    } else {
+                        flipX = false;
+                        flipY = false;
+                    }
+                }
+
+                batch.draw(textureRegion.getTexture(), actor.x - calculatedXOffset - xOffset + attackXOffset, actor.y + calculatedYOffset + yOffset, 0, 0, textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), 1, 1, rotation, textureRegion.getRegionX(), textureRegion.getRegionY(), textureRegion.getRegionWidth(), textureRegion.getRegionHeight(), flipX, flipY);
+                break;
+            case Dead:
+
+                break;
+            default:
+
+        }
+    }
+
+    private void calculateOffset(AbstractActor actor, boolean flip) {
+        if (attackDelay > 0) {
+            attackDelay--;
+
+        } else if(attackDelay == 0) {
+            attackDelay = -1;
+
+            if(flip) {
+                attackXOffset = -15;
+                rotation = 0;
+            } else {
+                attackXOffset = -15;
+                rotation = 0;
+            }
+
+        } else {
+
+            if (flip) {
+                attackXOffset -= 3f;
+
+                if (attackXOffset <= -40) {
+                    attackDelay = actor.getAttackSpeed();
+                    attackAnimation = new Random().nextInt(2);
+                    attackXOffset = -10;
+                    rotation = 0;
+                    flipY = false;
+                    // todo attack calc trigger here
+                }
+            } else {
+                attackXOffset += 3f;
+
+                if (attackXOffset >= 15) {
+                    attackDelay = actor.getAttackSpeed();
+                    attackAnimation = new Random().nextInt(2);
+                    attackXOffset = -10;
+                    rotation = 0;
+                    flipY = false;
+                    // todo attack calc trigger here
+                }
+            }
+        }
+    }
+
+    private void calculateRotation(AbstractActor actor, boolean flip) {
+        if (attackDelay > 0) {
+            attackDelay--;
+
+        } else if(attackDelay == 0) {
+            attackDelay = -1;
+            if(!flip) {
+                attackXOffset = 0;
+                rotation = -250;
+            } else {
+                attackXOffset = 0;
+                rotation = -290;
+            }
+        } else {
+            if (!flip) {
+                rotation -= 9f;
+                if (rotation <= -380) {
+
+                    attackDelay = actor.getAttackSpeed();
+                    attackAnimation = new Random().nextInt(2);
+                    attackXOffset = -10;
+                    rotation = 0;
+                    // todo attack calc trigger here
+                }
+            } else {
+                rotation += 9f;
+                if (rotation >= -160) {
+
+                    attackDelay = actor.getAttackSpeed();
+                    attackAnimation = new Random().nextInt(2);
+
+                    attackXOffset = -10;
+                    rotation = 0;
+                    flipX = true;
+                    flipY = false;
+                    // todo attack calc trigger here
+                }
+            }
+        }
+    }
+}
